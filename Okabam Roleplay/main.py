a004b2e820b5b0c5bc2ef889ee603cd3e20c2ccf
Antonio Netto <37848607+vasconcellosnetto@users.noreply.github.com>
2022-03-12 15:30:33 -0300

    Add files via upload

--- Okabam Roleplay/main.py
+++ Okabam Roleplay/main.py
@@ -34,13 +34,8 @@ async def reload(ctx, extension):
 @client.event
 async def on_raw_reaction_add(payload):
 
-  def check_ticket(reaction, user_ticket):
-    return (discord.utils.get(guild.roles, name="STAFF") in user_ticket.roles or discord.utils.get(guild.roles, name="SUPORTE") in user_ticket.roles or discord.utils.get(guild.roles, name="DIRETOR") in user_ticket.roles or user == user_ticket) and my_msg == reaction.message
-    #return user == user_ticket and my_msg == reaction.message
-
   def check_report(reaction, user_report):
-    return (discord.utils.get(guild.roles, name="STAFF") in user_report.roles or discord.utils.get(guild.roles, name="SUPORTE") in user_report.roles or discord.utils.get(guild.roles, name="DIRETOR") in user_report.roles or user == user_report) and my_msg == reaction.message
-    #return user == user_report and my_msg == reaction.message
+    return user == user_report and my_msg == reaction.message
 
   channel = await client.fetch_channel(payload.channel_id)
   message = await channel.fetch_message(payload.message_id)
@@ -58,36 +53,33 @@ async def on_raw_reaction_add(payload):
     } 
 
     if str(reaction) == "🆘":
-      channel_ticket = await guild.create_text_channel(f'🎫│𝚃𝚒𝚌𝚔𝚎𝚝 {user.name}', overwrites = overwrites, category = discord.utils.get(guild.categories, name='Tickets'))
+      channel_ticket = await guild.create_text_channel(f'🎫│𝚃𝚒𝚌𝚔𝚎𝚝 {user.name}',overwrites = overwrites, category = discord.utils.get(guild.categories, name='Tickets'))
       
       await reaction.remove(payload.member)
-      embed = discord.Embed(description = '〽️ Para adicionar outra pessoa no ticket, use o comando !add <id>\n\n〽️ Para remover uma pessoa do ticket, use o comando !rem <id>\n\n〽️ Para finalizar o ticket, clique no ❌ abaixo.', color = discord.Color.orange())
+      embed = discord.Embed(description = '〽️ Para adicionar outra pessoa no ticket, use o comando !adc <id>\n\n〽️ Para remover uma pessoa do ticket, use o comando !rem <id>\n\n〽️ Para finalizar o ticket, use o comando !fechar', color = discord.Color.orange())
       embed.set_footer(text = '🦅 | Okabam Roleplay')
       my_msg = await channel_ticket.send(user.mention, embed = embed)
-
-      await channel_ticket.set_permissions(discord.utils.get(guild.roles, name="STAFF"), read_message=True)
-      await channel_ticket.set_permissions(discord.utils.get(guild.roles,  name="SUPORTE"), read_message=True)
-      await my_msg.add_reaction('❌')
-      await client.wait_for('reaction_add', check=check_ticket)    
-      await client.wait_until_ready()
-      await channel_ticket.delete() 
+      
+      await channel_ticket.set_permissions(discord.utils.get(guild.roles, name="STAFF"), read_messages=True)
+      await channel_ticket.set_permissions(discord.utils.get(guild.roles,  name="SUPORTE"), read_messages=True)
 
     if str(reaction) == "⚠️":
       repN = len(discord.utils.get(guild.categories, name='Denúncias').channels) + 1
 
-      channel_report = await guild.create_text_channel(f'🛑│𝙳𝚎𝚗𝚞𝚗𝚌𝚒𝚊 n° {repN}', overwrites = overwrites, category = discord.utils.get(guild.categories, name='Denúncias'), sync_permissions=True)
+      channel_report = await guild.create_text_channel(f'🛑│𝙳𝚎𝚗𝚞𝚗𝚌𝚒𝚊 n° {repN}', overwrites = overwrites, category = discord.utils.get(guild.categories, name='Denúncias'))
       
       await reaction.remove(payload.member)
       embed = discord.Embed(description = '〽️ Com detalhes, explique o ocorrido/motivo da denúncia.\n\n〽️ Encaminhe todas as provas possíveis do ocorrido. Ao finalizar, clique no ✅ abaixo. ', color = discord.Color.gold())
       embed.set_footer(text = '🦅 | Okabam Roleplay')    
       my_msg = await channel_report.send(user.mention, embed = embed)
 
-      await channel_report.set_permissions(discord.utils.get(guild.roles, name="STAFF"), read_message=True)
-      await channel_report.set_permissions(discord.utils.get(guild.roles,  name="SUPORTE"), read_message=True)
+      await channel_report.set_permissions(discord.utils.get(guild.roles, name="STAFF"), read_messages=True)
+      await channel_report.set_permissions(discord.utils.get(guild.roles,  name="SUPORTE"), read_messages=True)  
       await my_msg.add_reaction('✅')
       await client.wait_for('reaction_add', check=check_report)    
       await client.wait_until_ready()
       await channel_report.set_permissions(payload.member, read_messages=False, send_messages=False)
+     
     
 @client.event
 async def on_message(message):
